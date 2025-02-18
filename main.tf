74c7100edbc2d347db1da088de4afdfb0cffd0c9
Gautam Limbani <gautamlimbani@yahoo.com>
2025-02-18 14:54:24 +0530

    instance error as count is used

--- main.tf
+++ main.tf
@@ -46,7 +46,7 @@ resource "aws_iam_policy_attachment" "user_policy_attachment" {
   count = length(data.aws_iam_user.existing_user.id) > 0 ? 1 : 0
   name      = "my_policy_attachment"
   users     = [data.aws_iam_user.existing_user.user_name]
-  policy_arn = aws_iam_policy.my_policy.arn
+  policy_arn = aws_iam_policy.my_policy[0].arn
 }
 
 # Ensure the policy is detached first before destroying it
@@ -54,7 +54,7 @@ resource "aws_iam_policy_attachment" "detach_user_policy" {
   count = length(data.aws_iam_user.existing_user.id) > 0 ? 1 : 0
   name      = "detach_my_policy"
   users     = [data.aws_iam_user.existing_user.user_name]
-  policy_arn = aws_iam_policy.my_policy.arn
+  policy_arn = aws_iam_policy.my_policy[0].arn
   lifecycle {
     prevent_destroy = false
   }
